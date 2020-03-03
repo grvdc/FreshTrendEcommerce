8ae849c09afb13b96e0bf98dda65e301a23783ef
gaurav-ocodewire <gauravk@ocodewire.com>
2020-03-03 12:42:28 +0530

    after-checkout validation

--- src/app/pages/after-cart/after-cart.page.ts
+++ src/app/pages/after-cart/after-cart.page.ts
@@ -11,6 +11,23 @@ export class AfterCartPage implements OnInit {
   totalAmount:any;
   cartItems = [];
   height : any;
+  check :boolean = false;
+  emailtest: boolean = false;
+  form = {
+    email: '',
+    fName  : '',
+    lName  : '',
+    company : '',
+    address : '',
+    appartment : '',
+    city : '',
+    country: '',
+    state : '',
+    pin: '',
+    mobile : '',
+  }
+
+
   constructor(
     private router: Router,
   ) { }
@@ -35,7 +52,7 @@ export class AfterCartPage implements OnInit {
       })
     }
     this.cartItems = a;
-    this.height = ((this.cartItems.length * 80)+258) + 'px';
+    this.height = ((this.cartItems.length * 72)+248) + 'px';
     console.log('cartItems', this.cartItems);
 
   }
@@ -47,5 +64,61 @@ export class AfterCartPage implements OnInit {
   cartPage(){
     this.router.navigateByUrl('/cart-page');
   }
+
+
+  recheck(){
+    console.log("Fname", this.form);
+    this.check = true;
+    this.form.fName = this.form.fName.trim();
+    if(!this.emailvalidate()){
+      this.emailtest = true;
+      return false;
+    }
+    if(this.form.fName == ''){
+      return false;
+    } else if(this.form.lName == ''){
+      return false;
+    }else if(this.form.address == ''){
+      return false;
+    }else if(this.form.city == ''){
+      return false;
+    }else if(this.form.country == ''){
+      return false;
+    }else if(this.form.pin == ''){
+      return false;
+    }else if(this.form.state == ''){
+      return false;
+    } else {
+      this.check = false;
+      return true;
+    }
+  }
+
+  emailvalidate(){
+    console.log('keypress');
+    if(this.form.email == ''){
+      this.emailtest = true;
+      // return false;
+    } else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.form.email)){
+      console.log("inside the Email validation");
+      this.emailtest = true;
+      // return false;
+    } else {
+      this.emailtest = false;
+      return true;
+    }
+  }
+  validate(){
+    if(this.recheck()){
+      console.log("testing Clear");
+    } else{
+      console.log("testing Stuck");
+
+    }
+  }
+
+  // else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.form.email)){
+  //   return false;
+  // }
   
 }
